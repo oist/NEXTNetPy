3dfc2988444a2548adb5a7018341fd2163a1b967
Sam <samuel.cure@me.com>
2023-03-31 20:07:53 +0900

    created cmakefile

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <pybind11/pybind11.h>
+#include <epidemics/epidemics/graph.h>
+#include <iostream>
+
+
+namespace py = pybind11;
+
+double f(double x, double y){
+    return x+y;
+}
+
+
+
+PYBIND11_MODULE(episimpy, handle) {
+    handle.doc() = "pybind11 example plugin"; // optional module docstring
+
+    handle.def("f", &f, "A function that adds two numbers");
+}
