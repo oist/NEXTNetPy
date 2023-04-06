cd3b6e834d1d7012ce47d885ea8ae0a34dcbc7a3
Samuel Cure <samuelcyrus.cure@oist.jp>
2023-04-06 22:06:35 +0900

    testing Next reaction

--- main.cpp
+++ main.cpp
@@ -1,12 +1,46 @@
-#include <pybind11/pybind11.h>
-#include "NextReaction.h"
 #include <iostream>
+#include <pybind11/pybind11.h>
+#include "stdafx.h"
+#include "random.h"
+#include "analysis.h"
+#include "NextReaction.h"
 
 
 namespace py = pybind11;
+rng_t engine;
 
-double f(double x, double y){
-    return x+y;
+
+double f(int r0,int size){
+    
+    const double MEAN_INFECTION = 10;
+    const double VARIANCE_INFECTION = 1;
+    const bool SHUFFLE_NEIGHBOURS = false;
+    const bool EDGES_CONCURRENT = true;
+
+    std::uniform_int_distribution<> dis(0, size-1);
+    std::uniform_real_distribution<> ran(0.0,1.0);
+
+    erdos_reyni network(size,r0,engine);
+
+    transmission_time_gamma psi(MEAN_INFECTION, VARIANCE_INFECTION);
+    simulate_next_reaction simulation(network, psi,nullptr,SHUFFLE_NEIGHBOURS,EDGES_CONCURRENT,false);
+
+    for (node_t i = 0; i < 1; i++)
+    {
+        node_t rand_node = dis(engine);
+        // const double thermal =-log(1-ran(engine))/Lambda;
+        // simulation.add_thermal_infections({ std::make_pair(rand_node, 0)},Lambda_r2,engine);
+        simulation.add_infections({ std::make_pair(rand_node, 0)});
+    }
+                        
+    while (true) {
+        auto point = simulation.step(engine);
+        if (!point )
+            break;
+        all_times.push_back(point->time);
+        
+    }
+    
 }
 
 
