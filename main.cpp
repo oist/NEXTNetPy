6c4e9ff268d7b4314ce0258acc77c94feb7b872f
Samuel Cure <samuelcyrus.cure@oist.jp>
2023-04-07 13:15:13 +0900

    the python module can now run an epidemic on any networkx graph

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h> 
 #include "stdafx.h"
 #include "random.h"
 #include "analysis.h"
@@ -9,38 +10,74 @@
 namespace py = pybind11;
 rng_t engine;
 
+std::vector<std::vector<int>> f(py::list py_list) {
+    std::vector<std::vector<int>> cpp_vector;
 
-double f(int r0,int size){
+    for (auto row : py_list) {
+        std::vector<int> cpp_row;
+        for (auto element : row) {
+            cpp_row.push_back(py::cast<int>(element));
+        }
+        cpp_vector.push_back(cpp_row);
+    }
+
+    return cpp_vector;
+}
+
+class networkx : public virtual graph_adjacencylist {
+public:
+    networkx(py::list py_list){
+
+    for (auto row : py_list) {
+        std::vector<int> cpp_row;
+        for (auto element : row) {
+            cpp_row.push_back(py::cast<node_t>(element));
+        }
+        adjacencylist.push_back(cpp_row);
+    }
+
+    }
+};
+
+
+
+std::vector<double> run_simulation(py::list adjacency_list, int seed = 1){
     
+    engine.seed(seed);
+
+    networkx network(adjacency_list);
+
+    const int SIZE = (int) network.adjacencylist.size();
+
+    const int INITIAL_INFECTED = 1;
     const double MEAN_INFECTION = 10;
     const double VARIANCE_INFECTION = 1;
     const bool SHUFFLE_NEIGHBOURS = false;
     const bool EDGES_CONCURRENT = true;
 
-    std::uniform_int_distribution<> dis(0, size-1);
-    std::uniform_real_distribution<> ran(0.0,1.0);
-
-    erdos_reyni network(size,r0,engine);
-
     transmission_time_gamma psi(MEAN_INFECTION, VARIANCE_INFECTION);
     simulate_next_reaction simulation(network, psi,nullptr,SHUFFLE_NEIGHBOURS,EDGES_CONCURRENT,false);
 
-    for (node_t i = 0; i < 1; i++)
+    std::vector<double> trajectory({});
+
+    std::uniform_int_distribution<> uniform_node_distribution(0, SIZE-1);
+
+
+    for (node_t i = 0; i < INITIAL_INFECTED; i++)
     {
-        node_t rand_node = dis(engine);
-        // const double thermal =-log(1-ran(engine))/Lambda;
-        // simulation.add_thermal_infections({ std::make_pair(rand_node, 0)},Lambda_r2,engine);
-        simulation.add_infections({ std::make_pair(rand_node, 0)});
+        node_t random_node = uniform_node_distribution(engine);
+        simulation.add_infections({ std::make_pair(random_node, 0)});
     }
                         
     while (true) {
         auto point = simulation.step(engine);
         if (!point )
             break;
-        all_times.push_back(point->time);
+        trajectory.push_back(point->time);
         
     }
-    
+
+    return trajectory;
 }
 
 
@@ -49,5 +86,6 @@ double f(int r0,int size){
 PYBIND11_MODULE(episimpy, handle) {
     handle.doc() = "pybind11 example plugin"; // optional module docstring
 
-    handle.def("f", &f, "A function that adds two numbers");
+    handle.def("convert",&f);
+    handle.def("simulate", &run_simulation, py::arg("adjacency_list"), py::arg("seed")=0, "A function that runs a SI epidemic on a graph G.");
 }
