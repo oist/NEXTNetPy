bbaa1fd6853ef15a793ff333179303030015107c
Sam <samuel.cure@me.com>
2023-06-19 13:15:42 +0900

    added tests written in python

---
+++ tests/test.py
@@ -0,0 +1,15 @@
+
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import nmepinet as nmen
+
+SIZE = 1000
+SIM = 100
+SEED = 1
+
+
+#
+# knn,pk = nmen.knn_depletion(SIZE,SIM,SEED)
+#
+# plt.plot()
